89e8cf10099acbfbce9453922d09b726e1d5da7a
Takács Vilmos <tvili999@gmail.com>
2021-08-15 22:06:44 +0200

    added notifiers

--- src/components/entries.js
+++ src/components/entries.js
@@ -3,6 +3,8 @@ module.exports = (container) => {
     let _entries = {}
     let _loadQueue = [];
 
+    let __notifiers = {};
+
     const api = {
         add: (name, loader) => {
             if(name in _loaders || name in _entries)
@@ -16,14 +18,27 @@ module.exports = (container) => {
             if(!(key in _loaders))
                 throw `No entry '${key}' in container`;
 
+            if(_loadQueue[_loadQueue.length - 1] === key) {
+                return await new Promise(resolve => {
+                    const wrapper = (...args) => {
+                        __notifiers[key] = __notifiers[key].filter(x => x !== wrapper);
+                        resolve(...args);
+                    }
+
+                    __notifiers[key] = [...(__notifiers[key] || []), wrapper];
+                });
+            }
+
             if(_loadQueue.includes(key))
                  throw "Circular module dependency: " + _loadQueue.join(" -> ") + " -> " + key;
 
             log(`Load module: ${key}`);
             
-            _loadQueue.push(key);
+            _loadQueue = [..._loadQueue, key];
             _entries[key] = await Promise.resolve(_loaders[key](container));
             _loadQueue = _loadQueue.filter(x => x !== key);
+            for(const handler of (__notifiers[key] || []))
+                handler(_entries[key]);
             return _entries[key];
         }
     };
